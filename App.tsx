b7ec648bace726331a7ec5b2fa4f445228f62475
Debabrata Batabyal <dbatabyal2018@gmail.com>
2024-11-08 16:25:40 +0530

    Touch movement and player

--- App.tsx
+++ App.tsx
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import {
+  BlendMode,
+  Canvas,
+  Circle,
+  createPicture,
+  Group,
+  Picture,
+  Skia,
+  SkiaDomView,
+} from "@shopify/react-native-skia";
+import { Dimensions } from "react-native";
+import { Player } from "./src/Entities/Player";
+import { Projectile } from "./src/Entities/Projectile";
+import {
+  makeMutable,
+  runOnJS,
+  useFrameCallback,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+const App = () => {
+  let canvasRef = useRef<SkiaDomView>(null);
 
-export default function App() {
+  const Window = Dimensions.get("window");
+  const cWidth = Window.width;
+  const cHeight = Window.height;
+  const projectiles: Projectile[] = [];
+  const player = new Player({
+    x: makeMutable(cWidth / 2),
+    y: makeMutable(cHeight / 2),
+    radius: 30,
+    color: "red",
+  });
+
+  function drawPlayer() {
+    return (
+      <Circle
+        cx={player.x}
+        cy={player.y}
+        r={player.radius}
+        color={player.color}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Canvas
+      onTouch={(e) => {
+        if (e[0][0].type == 2) {
+          const x = makeMutable(e[0][0].x);
+          const y = makeMutable(e[0][0].y);
+          const newProjectile = new Projectile({
+            x,
+            y,
+            radius: 10,
+            color: "blue",
+            velocity: { x: 5, y: 5 },
+          });
+          console.log("event", e, projectiles);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          projectiles.push(newProjectile);
+          player.x.value = withTiming(e[0][0].x);
+          player.y.value = withTiming(e[0][0].y);
+        }
+      }}
+      ref={canvasRef}
+      style={{ height: cHeight, width: cWidth }}
+    >
+      {drawPlayer()}
+    </Canvas>
+  );
+};
+export default App;
